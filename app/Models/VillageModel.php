3f3bb71b48c223094151885af7f29fd5b473bc68
wangbin <891699279@qq.com>
2023-10-29 20:25:11 +0800

    小区

--- app/Models/VillageModel.php
+++ app/Models/VillageModel.php
@@ -10,17 +10,4 @@ class VillageModel extends Model
 
     public $timestamps = false;
 
-
-    public function floor()
-    {
-        return $this->hasMany(VillageFloorModel::class, 'village_id');
-    }
-
-    public function recycler() {
-        return $this->hasOne(RecyclerVillageModel::class, 'village_id');
-    }
-
-    public function site() {
-        return $this->belongsTo(GarbageSiteModel::class, 'site_id');
-    }
 }
---
+++ app/Services/User/VillageService.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Services\User;
+
+use App\Models\VillageModel;
+
+class VillageService
+{
+    /**
+     * @param $data
+     * @return bool
+     * @throws \Throwable
+     */
+    public function createVillage($data)
+    {
+        VillageModel::query()->insert([
+            'name' => $data['name'],
+            'province' => $data['province'],
+            'city' => $data['city'],
+            'area' => $data['area'],
+            'is_active' => $data['is_active'],
+            'type' => $data['type']
+        ]);
+
+        return true;
+    }
+
+    /**
+     * @param $id
+     * @param $data
+     * @throws \Throwable
+     */
+    public function updateVillage($id, $data)
+    {
+        VillageModel::query()->whereKey($id)->update([
+            'name' => $data['name'],
+            'is_active' => $data['is_active'],
+            'type' => $data['type']
+        ]);
+
+        return true;
+    }
+
+    /**
+     * 通过地区和小区名搜索小区列表
+     *
+     * @param $area
+     * @param string $name
+     * @return array
+     */
+    public function getVillageByArea($area, $name = '')
+    {
+        $where['area'] = $area;
+        if (!empty($name)) {
+            $where['name'] = '%' . $name . '%';
+        }
+        return VillageModel::query()->where($where)->get()->toArray();
+    }
+
+
+    public function getVillageList(
+        $where = [],
+        $select = ['*'],
+        $page = 0,
+        $pageSize = 0,
+        $withPage = false
+    )
+    {
+        return VillageModel::query()->macroQuery($where, $select, [], $page, $pageSize, $withPage);
+    }
+}
