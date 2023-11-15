72ebe8968a86bbb124bea02c1ec93e3715642d3c
googol24 <sa514132@mail.ustc.edu.cn>
2023-11-15 20:54:46 +0800

    1x

--- app/Http/Controllers/Official/IndexController.php
+++ app/Http/Controllers/Official/IndexController.php
@@ -20,7 +20,8 @@ class IndexController extends BaseController
         $data['announcement'] = app(ConfigService::class)->getConfig(ConfigConst::USER_ANNOUNCEMENT);
         $data['garbage_types'] = [];
         $data['jifen_items'] = app(JifenItemService::class)->getAppointList(6);
-        $data['order_no'] = generate_order_no(1, 'R');
+        $data['order_no'] = generate_order_no('R');
+        $data['7dates'] = date('Y-m-d', strtotime('+7 day'));
 
         return $this->success($data);
     }
